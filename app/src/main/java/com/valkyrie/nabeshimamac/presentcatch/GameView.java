3bf21db44213beccceb0a998937338d633098b17
YukiNabeshima <yukinabe3561@icloud.com>
2016-07-17 16:47:12 +0900

    Initial commit

---
+++ app/src/main/java/com/valkyrie/nabeshimamac/presentcatch/GameView.java
@@ -0,0 +1,180 @@
+package com.valkyrie.nabeshimamac.presentcatch;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+/**
+ * Created by NabeshimaMAC on 16/04/26.
+ */
+public class GameView extends SurfaceView implements SurfaceHolder.Callback ,Runnable{
+    Bitmap presentImage;
+
+    int score = 0;
+    int life = 10;
+
+    static final long FPS = 30;
+    static final long FRAME_TIME = 1000 / FPS;
+
+    SurfaceHolder surfaceHolder;
+    Thread thread;
+
+    Present present;
+
+    int screenWidth , screenHeight;
+
+    Player player;
+    Bitmap playerImage;
+
+
+    public GameView(Context context){
+        super(context);
+        getHolder().addCallback(this);
+
+        Resources resources = context.getResources();
+        presentImage = BitmapFactory.decodeResource(resources, R.drawable.img_present0);
+
+        playerImage = BitmapFactory.decodeResource(resources, R.drawable.img_play);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(presentImage,100,200,null);
+        holder.unlockCanvasAndPost(canvas);
+
+        surfaceHolder = holder;
+        thread = new Thread(this);
+        thread.start();
+
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        screenWidth = width;
+        screenHeight = height;
+
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        thread = null;
+
+    }
+
+    class Present{
+        private static final int WIDTH = 100;
+        private static final int HEIGHT = 100;
+
+        float x,y;
+        public Present(){
+            Random random = new Random();
+            x = random.nextInt(screenWidth - WIDTH);
+            y = 0;
+        }
+        public void update(){
+            y += 15.0f;
+        }
+
+        public void reset(){
+            Random random = new Random();
+            x = random.nextInt(screenWidth - WIDTH);
+            y = 0;
+        }
+    }
+
+
+    @Override
+    public void run(){
+        present = new Present();
+        player = new Player();
+
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setFakeBoldText(true);
+        textPaint.setTextSize(100);
+
+        while (thread != null){
+            Canvas canvas = surfaceHolder.lockCanvas();
+            canvas.drawColor(Color.WHITE);
+            canvas.drawBitmap(playerImage, player.x,player.y,null);
+
+            canvas.drawBitmap(presentImage,present.x, present.y, null);
+
+
+            if(player.isEnter(present)){
+                present.reset();
+                score += 10;
+            }else if (present.y > screenHeight){
+                present.reset();
+                life--;
+            }else {
+                present.update();
+            }
+            present.update();
+
+            surfaceHolder.unlockCanvasAndPost(canvas);
+
+            canvas.drawText("SCORE :" + score, 50, 150, textPaint);
+            canvas.drawText("LIFE :" + life, 50, 300, textPaint);
+
+            try {
+                Thread.sleep(FRAME_TIME);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            if(life <= 0){
+                canvas.drawText("Game Over", screenWidth / 3, screenHeight / 2, textPaint);
+                surfaceHolder.unlockCanvasAndPost(canvas);
+                break;
+            }
+
+
+
+
+        }
+
+    }
+
+    class Player{
+        final int WIDTH = 200;
+        final int HEIGHT = 200;
+
+        float x , y;
+
+        public Player(){
+            x = 0;
+            y = screenHeight - HEIGHT;
+        }
+
+        public void move(float diffX){
+            this.x += diffX;
+            this.x = Math.max(0,x);
+            this.x = Math.min(screenWidth - WIDTH, x);
+
+        }
+
+        public boolean isEnter(Present present){
+            if(present.x + Present.WIDTH > x && present.x < x + WIDTH &&
+                    present.y + Present.HEIGHT > y && present.y < y + HEIGHT){
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/valkyrie/nabeshimamac/presentcatch/MainActivity.java
@@ -0,0 +1,60 @@
+package com.valkyrie.nabeshimamac.presentcatch;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private GameView gameView;
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            Log.d("SensorValues", "\nX軸:" + event.values[0]+
+            "\nY軸:" + event.values[1] +
+            "\nZ軸:" + event.values[2]);
+
+            if(gameView.player != null){
+                gameView.player.move(-event.values[0]);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if(!sensors.isEmpty()){
+            sensorManager.registerListener(this, sensors.get(0), SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        sensorManager.unregisterListener(this);
+        super.onPause();
+    }
+}
+
